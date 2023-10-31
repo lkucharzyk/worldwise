10a35bc65249ad3c41f844fc210282dea225e9bd
Łukasze <lukaszkucharzyk17@gmail.com>
2023-10-31 13:41:11 +0100

    added countires list

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,31 @@
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return (
+      <Message message="Add Your first city by clicking on the city on the map" />
+    );
+
+  const countries = cities.reduce((acc, city) => {
+    if (!acc.map((el) => el.country).includes(city.country)) {
+      return [...acc, { country: city.country, emoji: city.emoji }];
+    } else {
+      return acc;
+    }
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem key={country.id} country={country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
